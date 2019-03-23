1109c373f42f450e220be21ef1e336c8ee415b35
Karly Mareka <karly.mareka@gmail.com>
2019-03-23 23:52:20 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color,COUNT(x) AS cnt FROM a
+  "SELECT color,COUNT(color) AS cnt FROM a
 GROUP BY x
 ORDER BY cnt DESC";
 end
